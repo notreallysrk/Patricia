c7cf283fb873b8b7d181b6d98a88f86ec7d76569
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-04 19:30:45 +0530

    Update connection_sql.py

--- ERICA/modules/sql/connection_sql.py
+++ ERICA/modules/sql/connection_sql.py
@@ -5,7 +5,7 @@ from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from tg_bot.modules.sql import SESSION, BASE
+from ERICA.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
