1c95f6fddcd61201ec915dbd60f7625f0728ca0d
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-04 14:49:40 +0530

    Update wallpaper.py

--- ERICA/modules/wallpaper.py
+++ ERICA/modules/wallpaper.py
@@ -1,10 +1,10 @@
 from random import randint
 
 import requests as r
-from tg_bot import WALL_API
+from ERICA import WALL_API
 from telegram import Update
 from telegram.ext import CallbackContext
-from tg_bot.modules.helper_funcs.decorators import kigcmd
+from ERICA.modules.helper_funcs.decorators import kigcmd
 
 # Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 
