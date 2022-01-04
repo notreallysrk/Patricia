12f7f3a266a385ad49ced5f754910169056116ca
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-04 16:17:38 +0530

    JAAN TE BHI JAYDA JAAN PYAAR DU

--- ERICA/modules/announce.py
+++ ERICA/modules/announce.py
@@ -4,10 +4,10 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from tg_bot.modules.log_channel import loggable
-from tg_bot.modules.helper_funcs.decorators import kigcmd
+from ERICA.modules.log_channel import loggable
+from ERICA.modules.helper_funcs.decorators import kigcmd
 
-import tg_bot.modules.sql.logger_sql as sql
+import ERICA.modules.sql.logger_sql as sql
 from ..modules.helper_funcs.anonymous import user_admin as u_admin, AdminPerms
 
 
