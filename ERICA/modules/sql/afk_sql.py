7bf58d3ef6ce6282a229eeff5fd113d35aa1407c
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-04 19:34:27 +0530

    Update afk_sql.py

--- ERICA/modules/sql/afk_sql.py
+++ ERICA/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, BigInteger
 
-from tg_bot.modules.sql import BASE, SESSION
+from ERICA.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
