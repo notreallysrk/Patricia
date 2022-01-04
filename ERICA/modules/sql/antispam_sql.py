03372933095685d192c2312241613d412c251efe
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-04 19:33:11 +0530

    Update antispam_sql.py

--- ERICA/modules/sql/antispam_sql.py
+++ ERICA/modules/sql/antispam_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Column, UnicodeText, String, Boolean
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from tg_bot.modules.sql import BASE, SESSION
+from ERICA.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
