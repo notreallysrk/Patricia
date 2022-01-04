7e8386832be0688f87129e912902f5840ea03695
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-04 20:26:42 +0530

    Update cleaner.py

--- ERICA/modules/cleaner.py
+++ ERICA/modules/cleaner.py
@@ -212,7 +212,7 @@ def bluetext_ignore_list(update: Update, context: CallbackContext):
     message.reply_text(text, parse_mode=ParseMode.HTML)
     return
 
-from tg_bot.modules.language import gs
+from ERICA.modules.language import gs
 
 def get_help(chat):
     return gs(chat, "cleaner_help")
