f7e4d753bca2430aad5840be5e5845299860a8c5
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-04 19:33:50 +0530

    Update antiflood_sql.py

--- ERICA/modules/sql/antiflood_sql.py
+++ ERICA/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText, BigInteger
 
-from tg_bot.modules.sql import SESSION, BASE
+from ERICA.modules.sql import SESSION, BASE
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
