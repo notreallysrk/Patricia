93229d41b558b4abb333d6b547dff907ca208642
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-04 15:43:57 +0530

    Umm

--- ERICA/modules/error_handler.py
+++ ERICA/modules/error_handler.py
@@ -5,7 +5,7 @@ from .helper_funcs.misc import upload_text
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
 from psycopg2 import errors as sqlerrors
-from tg_bot import KInit, dispatcher, DEV_USERS, OWNER_ID, log
+from ERICA import KInit, dispatcher, DEV_USERS, OWNER_ID, log
 
 class ErrorsDict(dict):
     "A custom dict to store errors and their count"
