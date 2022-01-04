d6422d60e6ccac2fdd7b85d936391a3fe55a32dc
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-04 15:03:41 +0530

    Update purge.py

--- ERICA/modules/purge.py
+++ ERICA/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from tg_bot import telethn
-from tg_bot.modules.helper_funcs.telethn.chatstatus import (
+from ERICA import telethn
+from ERICA.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages, user_is_admin)
 
 
