f878122d479ea0d5d9d99d88073052ebd7640a09
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-04 19:20:44 +0530

    Update rss_sql.py

--- ERICA/modules/sql/rss_sql.py
+++ ERICA/modules/sql/rss_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer
 
-from tg_bot.modules.sql import BASE, SESSION
+from ERICA.modules.sql import BASE, SESSION
 
 
 class RSS(BASE):
