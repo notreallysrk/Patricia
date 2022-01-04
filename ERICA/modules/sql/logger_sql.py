0d5f61d876f148abc76beb2490ac2e865aba6a66
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-04 19:25:47 +0530

    Update logger_sql.py

--- ERICA/modules/sql/logger_sql.py
+++ ERICA/modules/sql/logger_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from tg_bot.modules.sql import BASE, SESSION
+from ERICA.modules.sql import BASE, SESSION
 
 class LoggerSettings(BASE):
     __tablename__ = "chat_log_settings"
