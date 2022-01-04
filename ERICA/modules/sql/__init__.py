2552df4ba96af6ac099f5fd54fe41350cdb549fa
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-04 21:10:16 +0530

    Update __init__.py

--- ERICA/modules/sql/__init__.py
+++ ERICA/modules/sql/__init__.py
@@ -1,25 +1,19 @@
+
+
+
+
+from ERICA import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from ERICA import KInit, log
-
-DB_URI = "postgres://dnldonok:TJjru0b0Vm86ApLkQsprkb0ETKFRjdXI@tyke.db.elephantsql.com/dnldonok"
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8", echo=KInit.DEBUG)
-    log.info("[PostgreSQL] Connecting to database......")
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-try:
-    SESSION: scoped_session = start()
-except Exception as e:
-    log.exception(f'[PostgreSQL] Failed to connect due to {e}')
-    exit()
-   
-log.info("[PostgreSQL] Connection successful, session started.")
+SESSION = start()
