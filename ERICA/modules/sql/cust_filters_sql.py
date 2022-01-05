faca9cdf90b15be0b470466f247d9548d9bb6cf1
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-05 09:57:43 +0530

    Um

--- ERICA/modules/sql/cust_filters_sql.py
+++ ERICA/modules/sql/cust_filters_sql.py
@@ -1,6 +1,7 @@
+
 import threading
 
-from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
+from sqlalchemy import Column, String, UnicodeText, Boolean, distinct, func, Integer
 
 from ERICA.modules.helper_funcs.msg_types import Types
 from ERICA.modules.sql import BASE, SESSION
@@ -69,7 +70,7 @@ class CustomFilters(BASE):
         return bool(
             isinstance(other, CustomFilters)
             and self.chat_id == other.chat_id
-            and self.keyword == other.keyword
+            and self.keyword == other.keyword,
         )
 
 
@@ -95,7 +96,7 @@ class NewCustomFilters(BASE):
         return bool(
             isinstance(other, CustomFilters)
             and self.chat_id == other.chat_id
-            and self.keyword == other.keyword
+            and self.keyword == other.keyword,
         )
 
 
@@ -360,11 +361,19 @@ def __migrate_filters():
             print(str(x.chat_id), x.keyword, x.reply, file_type.value)
             if file_type == Types.TEXT:
                 filt = CustomFilters(
-                    str(x.chat_id), x.keyword, x.reply, file_type.value, None
+                    str(x.chat_id),
+                    x.keyword,
+                    x.reply,
+                    file_type.value,
+                    None,
                 )
             else:
                 filt = CustomFilters(
-                    str(x.chat_id), x.keyword, None, file_type.value, x.reply
+                    str(x.chat_id),
+                    x.keyword,
+                    None,
+                    file_type.value,
+                    x.reply,
                 )
 
             SESSION.add(filt)
@@ -384,11 +393,10 @@ def migrate_chat(old_chat_id, new_chat_id):
         for filt in chat_filters:
             filt.chat_id = str(new_chat_id)
         SESSION.commit()
-        try:
-            CHAT_FILTERS[str(new_chat_id)] = CHAT_FILTERS[str(old_chat_id)]
-        except KeyError:
-            pass
-        del CHAT_FILTERS[str(old_chat_id)]
+        old_filt = CHAT_FILTERS.get(str(old_chat_id))
+        if old_filt:
+            CHAT_FILTERS[str(new_chat_id)] = old_filt
+            del CHAT_FILTERS[str(old_chat_id)]
 
         with BUTTON_LOCK:
             chat_buttons = (
