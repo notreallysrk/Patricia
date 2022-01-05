f1e93c9a9fba6d286ffdc4ed4d4a7b20a88b2fc1
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-05 14:34:11 +0530

    Update chat_status.py

--- ERICA/modules/helper_funcs/chat_status.py
+++ ERICA/modules/helper_funcs/chat_status.py
@@ -187,7 +187,7 @@ def whitelist_plus(func):
             return func(update, context, *args, **kwargs)
         else:
             update.effective_message.reply_text(
-                f"You don't have access to use this.\nVisit @YorkTownEagleUnion"
+                f"You don't have access to use this.\nVisit @ERICA_UPDATES"
             )
 
     return is_whitelist_plus_func
