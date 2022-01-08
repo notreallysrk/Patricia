7435e011a5f8c4a11289e0200f26467a8f55399d
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-08 11:04:11 +0530

    Update special.py

--- ERICA/modules/special.py
+++ ERICA/modules/special.py
@@ -7,10 +7,10 @@ from telegram.ext import Filters, CommandHandler
 from telegram.ext.dispatcher import run_async, CallbackContext
 
 import random
-import VegetaRobot.modules.sql.users_sql as sql
-from VegetaRobot.modules.helper_funcs.filters import CustomFilters
-from VegetaRobot import dispatcher, OWNER_ID, LOGGER
-from VegetaRobot.modules.disable import DisableAbleCommandHandler
+import Erica.modules.sql.users_sql as sql
+from Erica.modules.helper_funcs.filters import CustomFilters
+from Erica import dispatcher, OWNER_ID, LOGGER
+from Erica.modules.disable import DisableAbleCommandHandler
 USERS_GROUP = 4
 
 @run_async
