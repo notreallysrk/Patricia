afa68eda521d56e5a099078e3c9b3d57685d0e04
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-26 13:35:26 +0530

    Update __init__.py

--- ERICA/__init__.py
+++ ERICA/__init__.py
@@ -151,12 +151,6 @@ else:
     
 telethn = TelegramClient(MemorySession(), APP_ID, API_HASH)
 dispatcher = updater.dispatcher
-ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
-try:
-    ubot2.start()
-
-
-
 # Load at end to ensure all prev variables have been set
 from ERICA.modules.helper_funcs.handlers import CustomCommandHandler
 
