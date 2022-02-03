c7d935877387a23cbec5973024af9b44771daca5
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-02-03 19:15:25 +0530

    Update muting.py

--- ERICA/modules/muting.py
+++ ERICA/modules/muting.py
@@ -111,7 +111,7 @@ def dmute(update: Update, context: CallbackContext) -> str:
     user_id, reason = extract_user_and_text(message, args)
     reply = check_user(user_id, bot, update)
     try:
-      prev = await bot.get_reply_message()
+      prev = bot.get_reply_message()
       await prev.delete()
     except Exception:
       pass
