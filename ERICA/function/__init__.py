1ba072727be3d6e93d0c80fdfa851521f2bbb170
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-02-03 14:30:38 +0530

    Update __init__.py

--- ERICA/function/__init__.py
+++ ERICA/function/__init__.py
@@ -1,9 +1,7 @@
 from telethon.tl import functions
 from telethon.tl import types
 from ERICA import telethn as tbot
-import Harita.modules.sql.elevated_sql as sql
-from Harita.modules.sql.chats_sql import add_chat, rmchat, is_chat, get_all_chat_id
-from Harita.modules.sql.setbio_sql import set_bio, rm_bio, check_bio_status, is_bio, get_all_bio_id
+import ERICA.modules.sql.nation_sql as sql
 
 
 async def is_admin(event, user):
