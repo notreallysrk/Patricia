05ed250d175ab7420f68f93307596ed0a3ac580d
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-02-25 13:15:27 +0530

    Update Admins.py

--- ERICA/modules/Admins.py
+++ ERICA/modules/Admins.py
@@ -102,6 +102,87 @@ def fullpromote(update: Update, context: CallbackContext) -> Optional[str]:
 
     return log_message
 
+@kigcmd(command=["lowpromote", "promote"], can_disable=False)
+@connection_status
+@bot_admin
+@can_promote
+@user_admin(AdminPerms.CAN_PROMOTE_MEMBERS)
+@loggable
+def fullpromote(update: Update, context: CallbackContext) -> Optional[str]:
+    bot = context.bot
+    args = context.args
+
+    message = update.effective_message
+    chat = update.effective_chat
+    user = update.effective_user
+
+    # promoter = chat.get_member(user.id)
+    """
+    if (
+            not (promoter.can_promote_members or promoter.status == "creator")
+            and not user.id in SUDO_USERS
+    ):
+        message.reply_text("You don't have the necessary rights to do that!")
+        return
+    """
+    user_id = extract_user(message, args)
+
+    if not user_id:
+        message.reply_text(
+            "You don't seem to be referring to a user or the ID specified is incorrect.."
+        )
+        return
+
+    try:
+        user_member = chat.get_member(user_id)
+    except:
+        return
+
+    if user_member.status in ("administrator", "creator"):
+        message.reply_text("How am I meant to promote someone that's already an admin?")
+        return
+
+    if user_id == bot.id:
+        message.reply_text("I can't promote myself! Get an admin to do it for me.")
+        return
+
+    # set same perms as bot - bot can't assign higher perms than itself!
+    bot_member = chat.get_member(bot.id)
+
+    try:
+        bot.promoteChatMember(
+            chat.id,
+            user_id,
+            can_change_info=bot_member.can_change_info,
+            can_post_messages=bot_member.can_post_messages,
+            can_edit_messages=bot_member.can_edit_messages,
+            can_delete_messages=bot_member.can_delete_messages,
+            can_invite_users=bot_member.can_invite_users,
+            can_pin_messages=bot_member.can_pin_messages,
+            can_manage_voice_chats=bot_member.can_manage_voice_chats,
+        )
+    except BadRequest as err:
+        if err.message == "User_not_mutual_contact":
+            message.reply_text("I can't promote someone who isn't in the group.")
+        else:
+            message.reply_text("An error occured while promoting.")
+        return
+
+    bot.sendMessage(
+        chat.id,
+        f"<b>{user_member.user.first_name or user_id}</b> was promoted by <b>{message.from_user.first_name}</b> in <b>{chat.title}</b>",
+        parse_mode=ParseMode.HTML,
+    )
+
+    log_message = (
+        f"<b>{html.escape(chat.title)}:</b>\n"
+        f"#PROMOTED\n"
+        f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+        f"<b>User:</b> {mention_html(user_member.user.id, user_member.user.first_name)}"
+    )
+
+    return log_message
+
 
 @kigcmd(command="demote", can_disable=False)
 @connection_status
@@ -178,13 +259,12 @@ def demote(update: Update, context: CallbackContext) -> Optional[str]:
         return
 
 
-"""
+
 @kigcmd(command="admincache", can_disable=False)
-@u_admin
 def refresh_admin(update, _):
     ADMIN_CACHE.pop(update.effective_chat.id)
     update.effective_message.reply_text("Admins cache refreshed!")
-"""
+
 
 
 @kigcmd(command="title", can_disable=False)
