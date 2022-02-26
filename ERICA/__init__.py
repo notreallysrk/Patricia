2c2c3345c3b5491bae163a83ef2b030aa8ec6fd8
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-02-26 19:55:23 +0530

    Update __init__.py

--- ERICA/__init__.py
+++ ERICA/__init__.py
@@ -157,11 +157,12 @@ DROP_UPDATES = True
 
 from ERICA.modules.sql import SESSION
 
-if DROP_UPDATES:
+if not KInit.DROP_UPDATES:
     updater = tg.Updater(token=TOKEN, base_url=KInit.BOT_API_URL, base_file_url=KInit.BOT_API_FILE_URL, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10}, persistence=PostgresPersistence(session=SESSION))
-
+    
 else:
     updater = tg.Updater(token=TOKEN, base_url=KInit.BOT_API_URL, base_file_url=KInit.BOT_API_FILE_URL, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10})
+    
 telethn = TelegramClient(MemorySession(), APP_ID, API_HASH)
 dispatcher = updater.dispatcher
 
