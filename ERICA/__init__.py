aa5728d620e86feae6c0c42ce72697b08ecbe1a9
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-02-26 20:36:59 +0530

    Update __init__.py

--- ERICA/__init__.py
+++ ERICA/__init__.py
@@ -4,6 +4,7 @@ import sys
 import time
 from typing import List
 import spamwatch
+from telethon.sessions import StringSession
 import telegram.ext as tg
 from telethon import TelegramClient
 from motor import motor_asyncio
