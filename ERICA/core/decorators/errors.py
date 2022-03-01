5b0a3d712137174f271cf2496acbc0504b2be642
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-01 09:37:10 +0530

    Update errors.py

--- ERICA/core/decorators/errors.py
+++ ERICA/core/decorators/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from Rose import OWNER_ID as LOG_GROUP_ID, pgram as app
+from ERICA import OWNER_ID as LOG_GROUP_ID, pgram as app
 
 
 def split_limits(text):
