28b6f2b3c49c49db8d4e9b94c344275764b1652c
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-16 17:34:41 +0530

    Update events.py

--- ERICA/events.py
+++ ERICA/events.py
@@ -1,6 +1,19 @@
-from telethon import events
-from ERICA import telethn, ubot2 as ubot
+import inspect
+import logging
+import sys
+import re
 
+from pathlib import Path
+from telethon import events
+
+from pymongo import MongoClient
+from ERICA import MONGO_DB_URI
+from ERICA import telethn
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["zaidrobot"]
+gbanned = db.gban
 
 def register(**args):
     """ Registers a new message. """
@@ -64,6 +77,74 @@ def callbackquery(**args):
     return decorator
 
 
+def bot(**args):
+    pattern = args.get("pattern")
+    r_pattern = r"^[/]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    reg = re.compile("(.*)")
+
+    if pattern is not None:
+        try:
+            cmd = re.search(reg, pattern)
+            try:
+                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
+            except BaseException:
+                pass
+
+            try:
+                FUN_LIST[file_test].append(cmd)
+            except BaseException:
+                FUN_LIST.update({file_test: [cmd]})
+        except BaseException:
+            pass
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.edit_date:
+                return
+            if check.fwd_from:
+                return
+            if check.is_group or check.is_private:
+                pass
+            else:
+                print("i don't work in channels")
+                return
+            if check.is_group:
+               if check.chat.megagroup:
+                  pass
+               else:
+                  print("i don't work in small chats")
+                  return
+                          
+            users = gbanned.find({})
+            for c in users:
+                if check.sender_id == c["user"]:
+                    return
+            try:
+                await func(check)
+                try:
+                    LOAD_PLUG[file_test].append(func)
+                except Exception:
+                    LOAD_PLUG.update({file_test: [func]})
+            except BaseException:
+                return
+            else:
+                pass
+
+        telethn.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
+
 def haritarobot(**args):
     pattern = args.get("pattern", None)
     disable_edited = args.get("disable_edited", False)
@@ -93,31 +174,3 @@ def haritarobot(**args):
     if pattern:
         if not ignore_unsafe:
             args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
-
-    def decorator(func):
-        async def wrapper(check):
-            if check.edit_date and check.is_channel and not check.is_group:
-                return
-            if group_only and not check.is_group:
-                return
-            if check.via_bot_id and not insecure and check.out:
-                # Ignore outgoing messages via inline bots for security reasons
-                return
-
-            try:
-                await func(check)
-            except events.StopPropagation:
-                raise events.StopPropagation
-            except KeyboardInterrupt:
-                pass
-            except BaseException as e:
-                print(e)
-            else:
-                pass
-
-        if not disable_edited:
-            ubot.add_event_handler(wrapper, events.MessageEdited(**args))
-        ubot.add_event_handler(wrapper, events.NewMessage(**args))
-        return wrapper
-
-    return decorator
