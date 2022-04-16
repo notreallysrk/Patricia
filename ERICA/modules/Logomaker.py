fe5099130f92d34d3c4be2d673bcef68e7a4dc65
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-16 17:41:08 +0530

    Update Logomaker.py

--- ERICA/modules/Logomaker.py
+++ ERICA/modules/Logomaker.py
@@ -10,205 +10,6 @@ API1 = "https://single-developers.up.railway.app/logo?name="
 API2 = "https://single-developers.up.railway.app/hqlogo?name="
 
 
-def is_admin(func):
-    @functools.wraps(func)
-    async def oops(client, message):
-        is_admin = False
-        try:
-            user = await message.chat.get_member(message.from_user.id)
-            admin_strings = ("creator", "administrator")
-            if user.status not in admin_strings:
-                is_admin = False
-            else:
-                is_admin = True
-
-        except ValueError:
-            is_admin = True
-        if is_admin:
-            await func(client, message)
-        else:
-            await message.reply("**Only Admins can execute this command!**")
-
-    return oops
-
-
-@pbot.on_message(filters.command("bglogo") & ~filters.edited & ~filters.bot)
-async def logo_maker(client, message):
-    if message.reply_to_message:
-        try:
-            msg = await client.send_message(
-                message.chat.id, "**Creating The Logo....**"
-            )
-        except:
-            return
-        try:
-            text = get_text(message.reply_to_message)
-        except:
-            return
-        if (
-            message.reply_to_message.video
-            or message.reply_to_message.document
-            or message.reply_to_message.photo
-            or message.reply_to_message.animation
-            or message.reply_to_message.sticker
-        ):
-            try:
-                await msg.edit("Sorry I can't get the text of replied message")
-                return
-            except:
-                return
-        if not text:
-            try:
-                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
-                return
-            except:
-                return
-        try:
-            req = requests.get(API1 + text.replace(" ", "%20"))
-        except:
-            return
-        try:
-            url = req.history[1].url
-        except:
-            return
-        try:
-            await msg.delete()
-        except:
-            return
-        try:
-            await message.reply_photo(
-                url,
-                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
-            )
-        except:
-            return
-
-    else:
-        try:
-            msg = await client.send_message(
-                message.chat.id, "**Creating The Logo....**"
-            )
-        except:
-            return
-        try:
-            text = get_text(message)
-        except:
-            return
-        if not text:
-            try:
-                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
-                return
-            except:
-                return
-        try:
-            req = requests.get(API1 + text.replace(" ", "%20"))
-        except:
-            return
-        try:
-            url = req.history[1].url
-        except:
-            return
-        try:
-            await msg.delete()
-        except:
-            return
-        try:
-            await message.reply_photo(
-                url,
-                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
-            )
-        except:
-            return
-
-
-@pbot.on_message(filters.command(["logohq", "wlogo"]) & ~filters.edited & ~filters.bot)
-async def logohq(client, message):
-    if message.reply_to_message:
-        try:
-            msg = await client.send_message(
-                message.chat.id, "**Creating The Logo....**"
-            )
-        except:
-            return
-        try:
-            text = get_text(message.reply_to_message)
-        except:
-            return
-        if (
-            message.reply_to_message.video
-            or message.reply_to_message.document
-            or message.reply_to_message.photo
-            or message.reply_to_message.animation
-            or message.reply_to_message.sticker
-        ):
-            try:
-                await msg.edit("Sorry I can't get the text of replied message")
-                return
-            except:
-                return
-        if not text:
-            try:
-                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
-                return
-            except:
-                return
-        try:
-            req = requests.get(API2 + text.replace(" ", "%20"))
-        except:
-            return
-        try:
-            url = req.history[1].url
-        except:
-            return
-        try:
-            await msg.delete()
-        except:
-            return
-        try:
-            await message.reply_photo(
-                url,
-                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
-            )
-        except:
-            return
-
-    else:
-        try:
-            msg = await client.send_message(
-                message.chat.id, "**Creating The Logo....**"
-            )
-        except:
-            return
-        try:
-            text = get_text(message)
-        except:
-            return
-        if not text:
-            try:
-                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
-                return
-            except:
-                return
-        try:
-            req = requests.get(API2 + text.replace(" ", "%20"))
-        except:
-            return
-        try:
-            url = req.history[1].url
-        except:
-            return
-        try:
-            await msg.delete()
-        except:
-            return
-        try:
-            await message.reply_photo(
-                url,
-                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
-            )
-        except:
-            return
-
 
 from ERICA.modules.language import gs
 
@@ -427,4 +228,220 @@ async def logo_(event):
     if os.path.exists(font_):
         if not font_.startswith("./Zaid/resources/fonts"):
             os.remove(font_)
+
+
+
+import functools
+
+import requests
+from pyrogram import filters
+
+from ERICA.function.pluginhelpers import get_text
+from ERICA import pgram as pbot
+
+API1 = "https://single-developers.up.railway.app/logo?name="
+API2 = "https://single-developers.up.railway.app/hqlogo?name="
+
+
+def is_admin(func):
+    @functools.wraps(func)
+    async def oops(client, message):
+        is_admin = False
+        try:
+            user = await message.chat.get_member(message.from_user.id)
+            admin_strings = ("creator", "administrator")
+            if user.status not in admin_strings:
+                is_admin = False
+            else:
+                is_admin = True
+
+        except ValueError:
+            is_admin = True
+        if is_admin:
+            await func(client, message)
+        else:
+            await message.reply("**Only Admins can execute this command!**")
+
+    return oops
+
+
+@pbot.on_message(filters.command("bglogo") & ~filters.edited & ~filters.bot)
+@is_admin
+async def logo_maker(client, message):
+    if message.reply_to_message:
+        try:
+            msg = await client.send_message(
+                message.chat.id, "**Creating The Logo....**"
+            )
+        except:
+            return
+        try:
+            text = get_text(message.reply_to_message)
+        except:
+            return
+        if (
+            message.reply_to_message.video
+            or message.reply_to_message.document
+            or message.reply_to_message.photo
+            or message.reply_to_message.animation
+            or message.reply_to_message.sticker
+        ):
+            try:
+                await msg.edit("Sorry I can't get the text of replied message")
+                return
+            except:
+                return
+        if not text:
+            try:
+                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
+                return
+            except:
+                return
+        try:
+            req = requests.get(API1 + text.replace(" ", "%20"))
+        except:
+            return
+        try:
+            url = req.history[1].url
+        except:
+            return
+        try:
+            await msg.delete()
+        except:
+            return
+        try:
+            await message.reply_photo(
+                url,
+                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
+            )
+        except:
+            return
+
+    else:
+        try:
+            msg = await client.send_message(
+                message.chat.id, "**Creating The Logo....**"
+            )
+        except:
+            return
+        try:
+            text = get_text(message)
+        except:
+            return
+        if not text:
+            try:
+                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
+                return
+            except:
+                return
+        try:
+            req = requests.get(API1 + text.replace(" ", "%20"))
+        except:
+            return
+        try:
+            url = req.history[1].url
+        except:
+            return
+        try:
+            await msg.delete()
+        except:
+            return
+        try:
+            await message.reply_photo(
+                url,
+                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
+            )
+        except:
+            return
+
+
+@pbot.on_message(filters.command(["logohq", "walllogo"]) & ~filters.edited & ~filters.bot)
+@is_admin
+async def logohq(client, message):
+    if message.reply_to_message:
+        try:
+            msg = await client.send_message(
+                message.chat.id, "**Creating The Logo....**"
+            )
+        except:
+            return
+        try:
+            text = get_text(message.reply_to_message)
+        except:
+            return
+        if (
+            message.reply_to_message.video
+            or message.reply_to_message.document
+            or message.reply_to_message.photo
+            or message.reply_to_message.animation
+            or message.reply_to_message.sticker
+        ):
+            try:
+                await msg.edit("Sorry I can't get the text of replied message")
+                return
+            except:
+                return
+        if not text:
+            try:
+                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
+                return
+            except:
+                return
+        try:
+            req = requests.get(API2 + text.replace(" ", "%20"))
+        except:
+            return
+        try:
+            url = req.history[1].url
+        except:
+            return
+        try:
+            await msg.delete()
+        except:
+            return
+        try:
+            await message.reply_photo(
+                url,
+                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
+            )
+        except:
+            return
+
+    else:
+        try:
+            msg = await client.send_message(
+                message.chat.id, "**Creating The Logo....**"
+            )
+        except:
+            return
+        try:
+            text = get_text(message)
+        except:
+            return
+        if not text:
+            try:
+                await msg.edit("**Invalid Command Syntax**\n\n`/logo [name]`")
+                return
+            except:
+                return
+        try:
+            req = requests.get(API2 + text.replace(" ", "%20"))
+        except:
+            return
+        try:
+            url = req.history[1].url
+        except:
+            return
+        try:
+            await msg.delete()
+        except:
+            return
+        try:
+            await message.reply_photo(
+                url,
+                caption=f"**Logo Generated Successfully** As {text}\n\nImage Link => {url}\n\n**By @Zaid2_Robot**",
+            )
+        except:
+            return
+
 
