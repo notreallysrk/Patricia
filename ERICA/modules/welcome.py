ad4ea1534d59ade35bc40df10c9ec2d35877ee84
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-20 10:36:23 +0530

    Update welcome.py

--- ERICA/modules/welcome.py
+++ ERICA/modules/welcome.py
@@ -27,7 +27,7 @@ from ERICA.modules.helper_funcs.string_handling import (
     markdown_parser,
 )
 from ERICA.modules.log_channel import loggable
-from ERICA.modules.sql.antiflood_sql import is_user_gbanned
+from ERICA.modules.sql.antispam_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
