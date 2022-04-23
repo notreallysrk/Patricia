d22e85f95d43d0ef815613f0e9bb0dace6804bab
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-23 23:26:33 +0530

    Update night_mode_sql.py

--- ERICA/modules/sql/night_mode_sql.py
+++ ERICA/modules/sql/night_mode_sql.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from ERICA.modules.sql import BASE, SESSION
+from sqlalchemy import Boolean, Column, String, UnicodeText
+from Yuriko.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
