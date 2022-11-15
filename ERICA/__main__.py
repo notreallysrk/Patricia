bde119fe3f2b378aae0da779bbf87c7802750fbd
Tricky Yash <110162515+TrickyYash198@users.noreply.github.com>
2022-11-15 23:00:23 +0530

    Update __main__.py

--- ERICA/__main__.py
+++ ERICA/__main__.py
@@ -237,11 +237,11 @@ def start(update: Update, context: CallbackContext):  # sourcery no-metrics
                         [
                             InlineKeyboardButton(
                                 text=gs(chat.id, "support_chat_link_btn"),
-                                url='https://t.me/TheSupportChat',
+                                url='https://t.me/TeamTrickyYash',
                             ),
                             InlineKeyboardButton(
                                 text=gs(chat.id, "updates_channel_link_btn"),
-                                url="https://t.me/TheUpdatesChannel",
+                                url="https://t.me/TrickyYash",
                             ),
                         ],
                         [
