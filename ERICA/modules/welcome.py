8ca36032bc5168aad4a5944002a6354f6dbb851f
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2023-01-04 23:15:25 +0530

    Update welcome.py

--- ERICA/modules/welcome.py
+++ ERICA/modules/welcome.py
@@ -256,7 +256,7 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
             # Welcome yourself
             elif new_mem.id == bot.id:
                 update.effective_message.reply_text(
-                    "Thanks for adding me! Join @TheSupportChat for support.",
+                    "Thanks for adding me! Join @TheDeadlyBots for support.",
                     reply_to_message_id=reply,
                 )
                 continue
